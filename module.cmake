ab45c97f4c1a1898380fc22b279faa2e3808ff40
Joachim Pouderoux <joachim.pouderoux@kitware.com>
2016-06-15 11:26:25 +0200

    Initial import

---
+++ module.cmake
@@ -0,0 +1,14 @@
+set(DOCUMENTATION "This modules introduces a VTK class to compute Delaunay triangulation in 2D using Shewchuck's Triangle.")
+
+vtk_module(vtkTriangleDelaunay2D
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  DEPENDS
+    vtkCommonDataModel
+    vtkCommonExecutionModel
+    vtkFiltersCore
+  TEST_DEPENDS
+    vtkTestingCore
+  KIT
+    vtkRemote
+)
