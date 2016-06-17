b4c7ab2ec4c1ed367736f594b776b88d4907057b
Joachim Pouderoux <joachim.pouderoux@kitware.com>
2016-06-17 17:08:40 +0200

    Update git tag

--- vtkTriangleDelaunay2D.remote.cmake
+++ vtkTriangleDelaunay2D.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(vtkTriangleDelaunay2D
   "2D Delaunay triangulation using Shewchuk's Triangle"
   GIT_REPOSITORY https://github.com/jpouderoux/vtkTriangleDelaunay2D
   # June 15, 2016 - first working as a remote module
-  GIT_TAG a2987c50db97403a2c32fc8bc83560bb8ef11fe4
+  GIT_TAG 8d77dd22515c44e5e62e4e0e522e7b2989203854
   )
